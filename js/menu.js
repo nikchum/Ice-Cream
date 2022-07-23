a717f4b60cf58d301de0162911b020b2022057af
nikchum <nchumak94@gmail.com>
2022-07-23 22:19:52 +0300

    Добавляет секцию hero-header

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,34 @@
 (() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
-
-  menuBtnRef.addEventListener("click", () => {
-    const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-
-    menuBtnRef.classList.toggle("is-open");
-    document.body.classList.toggle("modal-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
-
-    mobileMenuRef.classList.toggle("is-open");
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuCloseRef = document.querySelector('[data-menu-close]');
+  const menuLinkRef = document.querySelector('.js-close-menu');
+  console.log(menuLinkRef);
+  menuBtnRef.addEventListener('click', () => {
+    document.body.classList.toggle('menu-open');
   });
+
+  mobileMenuCloseRef.addEventListener('click', () => {
+    document.body.classList.toggle('menu-open');
+  });
+
+  menuLinkRef.addEventListener('click', () => {
+    document.body.classList.toggle('menu-open');
+  });
+
+  // Найти все ссылки начинающиеся на #
+  const anchors = document.querySelectorAll('a[href^="#"]');
+
+  // Цикл по всем ссылкам
+  for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body';
+      // Плавная прокрутка до элемента с id = href у ссылки
+      document.querySelector(goto).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    });
+  }
 })();
--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,23 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtnTablet: document.querySelector('[data-modal-open]'),
+    openModalBtnMob: document.querySelector('[data-modal-open-mobile]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtnTablet.addEventListener('click', toggleModal);
+  refs.openModalBtnMob.addEventListener('click', toggleModalMob);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    document.body.classList.toggle("modal-open");
-    refs.modal.classList.toggle("is-hidden");
+    document.body.classList.toggle('modal-open');
+    refs.modal.classList.toggle('is-hidden');
+  }
+
+  function toggleModalMob() {
+    document.body.classList.toggle('modal-open');
+    refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle('menu-open');
   }
 })();
